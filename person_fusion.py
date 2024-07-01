06527d34abc6f5947e447e0f2e85d384ef495cda
DMGamage <gamagemadhushan@gmail.com>
2024-07-01 19:08:42 +0530

    Create a output parser to convert LLM output to python object

--- person_fusion.py
+++ person_fusion.py
@@ -3,6 +3,7 @@ import os
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
+from output_parser import summary_parser
 
 from third_partie.linkedin import scrape_linkedin_profile
 from agents.linkedin_agent import lookup as linkedin_lookup_agent
@@ -17,7 +18,8 @@ def ice_break_with(name: str) -> str:
     2. two interesting facts about them
     """
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], template=summary_template,
+        partial_variables={"format_instructions":summary_parser.get_format_instructions()}
     )
 
     # Load the environment variables
@@ -28,6 +30,7 @@ def ice_break_with(name: str) -> str:
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key)
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data})
 
