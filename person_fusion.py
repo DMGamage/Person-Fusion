43137721f811cd74cf55b614c419ab094ada1ae1
DMGamage <gamagemadhushan@gmail.com>
2024-07-11 00:55:24 +0530

    FontEnd Complete

--- person_fusion.py
+++ person_fusion.py
@@ -1,41 +1,49 @@
+from typing import Tuple
+
 from dotenv import load_dotenv
-import os
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
-from output_parser import summary_parser
 
 from third_partie.linkedin import scrape_linkedin_profile
 from agents.linkedin_agent import lookup as linkedin_lookup_agent
+from output_parser import summary_parser, Summary
 
-def ice_break_with(name: str) -> str:
+
+def person_fusion_with(name: str) -> Tuple[Summary, str]:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
-
-    summary_template = """
-    given the Linkedin information {information} about a person I want you to create:
-    1. A short summary
-    2. two interesting facts about them
-    """
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template,
-        partial_variables={"format_instructions":summary_parser.get_format_instructions()}
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_username, mock=True
     )
 
-    # Load the environment variables
-    load_dotenv()
-    openai_api_key = os.getenv("OPENAI_API_KEY")
 
-    # Initialize the OpenAI API with the loaded API key
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key)
+    summary_template = """
+    given the information about a person from linkedin {information},
+    and their latest twitter posts {twitter_posts} I want you to create:
+    1. A short summary
+    2. two interesting facts about them 
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    Use both information from twitter and Linkedin
+    \n{format_instructions}
+    """
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information", "twitter_posts"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+        },
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
     chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    return res, linkedin_data.get("profile_pic_url")
+
 
 if __name__ == "__main__":
-    print("PersonFusion Started")
-    ice_break_with(name="Dhanushka Madhushan Gamage")
+    load_dotenv()
+
+    print("Ice Breaker Enter")
+    person_fusion_with(name="Harrison Chase")
